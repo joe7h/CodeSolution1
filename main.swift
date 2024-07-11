0d6e3f616bdbefd9fae88ab7a36582abd4f468ba
joe7h <holmejos000@gmail.com>
2024-07-11 14:02:28 -0400

    Create main.swift

---
+++ main.swift
@@ -0,0 +1,10 @@
+let appName : String = "TEEMS APP"
+
+var currentVersion : Double = 18.0
+
+print("My App Name: \(appName)")
+print("Current Version: \(currentVersion)")
+
+
+currentVersion += 0.1
+print("Current Version: \(currentVersion)")
